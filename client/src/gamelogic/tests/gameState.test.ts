70a68076d2d6d3bc1939b3128a48e7410f6aeafc
Teun van Dalen <tvdalen@sogyo.nl>
2023-11-13 12:01:07 +0100

    ci test

--- client/src/gamelogic/tests/gameState.test.ts
+++ client/src/gamelogic/tests/gameState.test.ts
@@ -4,3 +4,8 @@ test('New gamestate object is on waiting phase', () => {
     const game = new GameState();
     expect(game.getCurrentPhase()).toBe(Phase.WAITING);
 });
+
+
+test('Should pass', () => {
+    expect(true).toBe(true);
+});
