60f462a81e213cdcb58c5e8770f432f2c07e7b83
Teun van Dalen <tvdalen@sogyo.nl>
2023-11-15 11:33:57 +0100

    push the login box a bit up for easier interaction on mobile devices

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,7 +4,7 @@ function Home({ createRoom, joinRoom, setRoomCode }: { createRoom: MouseEventHan
 
     return (
         <div className='flex justify-center items-center h-screen'>
-            <div className='flex flex-col p-6 bg-white shadow-md space-y-4'>
+            <div className='mt-[-300px] flex flex-col p-6 bg-white shadow-md space-y-4'>
                 <input className='p-2 border border-gray-300' type='text' placeholder='room code' onChange={(e) => setRoomCode(e.target.value)} />
                 <button className='p-2 bg-blue-400 rounded-md text-white' onClick={joinRoom}>Join Room</button>
                 <button className='p-2 bg-green-400 rounded-md text-white' onClick={createRoom}>Create Room</button>
