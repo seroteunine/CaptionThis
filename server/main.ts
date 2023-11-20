7cb57ea3b3d153b8003ba166e6a569800f5a2ca9
Teun van Dalen <tvdalen@sogyo.nl>
2023-11-20 16:47:12 +0100

    change url cors back

--- server/main.ts
+++ server/main.ts
@@ -9,7 +9,7 @@ import { generateRoomID, generatePlayerID } from './utils';
 const server = http.Server(app)
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "http://teunvandalen.nl",
         methods: ["GET", "POST"]
     }
 });
