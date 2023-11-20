08c44d4556d6521f6d3dbabd6298e88a45c598c6
seroteunine <67736564+seroteunine@users.noreply.github.com>
2023-11-20 13:43:36 +0100

    Update socket.ts (#43)

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "http://10.10.2.105:5000";
+const URL = "http://teunvandalen.nl:5000";
 const socket = io(URL);
 
 socket.onAny((event, ...args) => {
