ddd72dcceff6443927b0b5fe547ea48f284cbcc6
Teun van Dalen <tvdalen@sogyo.nl>
2023-11-21 10:44:46 +0100

    myserviceurl process variable

--- server/main.ts
+++ server/main.ts
@@ -10,11 +10,10 @@ import { generateRoomID, generatePlayerID } from './utils';
 
 console.log(process.env.MY_SERVICE_URL);
 
-
 const server = http.Server(app)
 const io = new Server(server, {
     cors: {
-        origin: process.env.MY_SERVICE_URL ? process.env.MY_SERVICE_URL : "http://localhost:5174",
+        origin: process.env.MY_SERVICE_URL,
         methods: ["GET", "POST"]
     }
 });
