d21e57f458dfcbe3012c39f98e189ac6b195efa9
Teun van Dalen <tvdalen@sogyo.nl>
2023-11-21 10:41:13 +0100

    shoudl change on prodhyuction now

--- server/main.ts
+++ server/main.ts
@@ -14,7 +14,7 @@ console.log(process.env.MY_SERVICE_URL);
 const server = http.Server(app)
 const io = new Server(server, {
     cors: {
-        origin: process.env.MY_SERVICE_URL ? process.env.MY_SERVICE_URL : "http://localhost:5173",
+        origin: process.env.MY_SERVICE_URL ? process.env.MY_SERVICE_URL : "http://localhost:5174",
         methods: ["GET", "POST"]
     }
 });
