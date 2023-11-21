62a23a0f1ce43d29624eb66bf4f74f5273d4ae9a
Teun van Dalen <tvdalen@sogyo.nl>
2023-11-21 10:18:48 +0100

    test3

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "http://teunvandalen:5000";
+const URL = "http://teunvandalen.nl:5000";
 const socket = io(URL);
 
 socket.onAny((event, ...args) => {
