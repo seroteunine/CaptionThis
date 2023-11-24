85960647c89880c28c939fdab84271b7d5cf4736
Teun van Dalen <tvdalen@sogyo.nl>
2023-11-24 14:36:32 +0100

    try other method for image width

--- client/src/components/ImageComponent.tsx
+++ client/src/components/ImageComponent.tsx
@@ -18,7 +18,7 @@ function ImageComponent({ arrayBuffer }: { arrayBuffer: ArrayBuffer }) {
 
     return (
         <div className='flex justify-center items-center h-full'>
-            {imageUrl && <img className='w-3/4 md:w-1/4 h-auto' src={imageUrl} alt="Loaded from ArrayBuffer" />}
+            {imageUrl && <img className='max-w-md h-auto' src={imageUrl} alt="Loaded from ArrayBuffer" />}
         </div>
     );
 }
