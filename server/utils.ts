61a29ddde6bf2eac5a150ac83eef10919f6f6c99
seroteunine <teun.vandalen.tvd@gmail.com>
2023-11-29 15:32:54 +0100

    change roomcode to always digits

--- server/utils.ts
+++ server/utils.ts
@@ -1,8 +1,8 @@
 const uuid = require('uuid');
 
 export function generateRoomID(): string {
-    const randomUUID = uuid.v4().replace(/-/g, '');
-    return randomUUID.substr(0, 4);
+    const randomNum = Math.floor(Math.random() * 10000);
+    return String(randomNum).padStart(4, '0');
 }
 
 export function generatePlayerID(): string {
