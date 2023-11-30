579e12fb350a6ad54665671bee529fd5a48380a2
Teun van Dalen <tvdalen@sogyo.nl>
2023-11-30 12:08:09 +0100

    front-end: ability to delete a player via settings button

---
+++ client/src/components/host/Settings.tsx
@@ -0,0 +1,30 @@
+import { useNameContext } from "../../context/NamesContext";
+import { useRoom } from "../../context/RoomContext";
+
+function Settings() {
+
+    const { roomDTO } = useRoom();
+    const { nameMap } = useNameContext();
+
+    const handleRemovePlayer = (e: React.SyntheticEvent) => {
+        e.preventDefault();
+    }
+
+    return (
+        <div className="fixed-badge">
+            <div className="dropdown">
+                <button className="btn btn-secondary dropdown-toggle text-primary fs-3" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i className="bi bi-sliders"></i>
+                </button>
+                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                    <li><h6 className="dropdown-header">Remove player: </h6></li>
+                    {roomDTO!.playerIDs.map((playerID) => (
+                        <li key={playerID}><button className="dropdown-item  player-item" onClick={handleRemovePlayer}>{nameMap.get(playerID) || playerID}</button></li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Settings;
